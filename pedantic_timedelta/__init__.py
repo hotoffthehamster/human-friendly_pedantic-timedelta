3627fa107137ed554a82935b57756c386c0c0123
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-02-15 18:19:20 -0600

    Docs: Add Copyright headers.

--- pedantic_timedelta/__init__.py
+++ pedantic_timedelta/__init__.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
-#  vim:tw=0:ts=4:sw=4:et
-# Copyright: © 2015-2018 Landon Bouma.
-# License: GPLv3. See LICENSE.
 
-""""""
+# This file is part of 'human-friendly_pedantic-timedelta'.
+#
+# 'human-friendly_pedantic-timedelta' is free software: you can re-
+# distribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# 'human-friendly_pedantic-timedelta' is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+# the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'human-friendly_pedantic-timedelta'. If not, visit:
+#
+#   http://www.gnu.org/licenses/
+
+"""A Human-friendly Pedantic `timedelta` formatter"""
 
 from __future__ import absolute_import, unicode_literals
 
--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# This file is part of 'human-friendly_pedantic-timedelta'.
+#
+# 'human-friendly_pedantic-timedelta' is free software: you can re-
+# distribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# 'human-friendly_pedantic-timedelta' is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+# the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'human-friendly_pedantic-timedelta'. If not, visit:
+#
+#   http://www.gnu.org/licenses/
 
 """
 Packaging instruction for setup tools.
