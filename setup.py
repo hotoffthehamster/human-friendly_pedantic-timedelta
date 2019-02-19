687b42da0d1bfc129395b1d70a72968aa4cb3276
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-02-18 22:13:43 -0600

    Version: Bump version: 0.0.6a1 → 0.0.6.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on single-
     # sourcing the version across setup.py and the project code, see
     #  https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.6a1',
+    version='0.0.6',
 
     description="Human-friendly Pedantic `timedelta`",
     long_description=long_description,
