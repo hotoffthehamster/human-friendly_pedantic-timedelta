11abebe0937ccee7f08a394d31746f3f9d5c05eb
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-01-24 23:54:17 -0600

    License: Release under MIT License.

---
+++ docs/license.rst
@@ -0,0 +1,16 @@
+#######
+LICENSE
+#######
+
+This software —
+``human-friendly_pedantic-timedelta`` —
+is Copyright © 2019-2020 Landon Bouma.
+All rights reserved.
+
+This software —
+``human-friendly_pedantic-timedelta`` —
+is licensed under the MIT License.
+
+.. literalinclude:: ../LICENSE
+   :language: none
+
--- tests/test_pedantic_timedelta.py
+++ tests/test_pedantic_timedelta.py
@@ -1,19 +1,26 @@
-# This file is part of 'human-friendly_pedantic-timedelta'.
+# This file exists within 'human-friendly_pedantic-timedelta' aka 'pedantic_timedelta':
 #
-# 'human-friendly_pedantic-timedelta' is free software: you can re-
-# distribute it and/or modify it under the terms of the GNU General
-# Public License as published by the Free Software Foundation, either
-# version 3 of the License, or (at your option) any later version.
+#   https://github.com/hotoffthehamster/human-friendly_pedantic-timedelta
 #
-# 'human-friendly_pedantic-timedelta' is distributed in the hope that
-# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-# the GNU General Public License for more details.
+# Copyright © 2018-2020 Landon Bouma. All rights reserved.
 #
-# You should have received a copy of the GNU General Public License
-# along with 'human-friendly_pedantic-timedelta'. If not, visit:
+# Permission is hereby granted,  free of charge,  to any person obtaining a
+# copy of this software and associated documentation files (the ),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge,  publish,  distribute, sublicense,
+# and/or  sell copies  of the Software,  and to permit persons  to whom the
+# Software  is  furnished  to do so,  subject  to the following conditions:
 #
-#   http://www.gnu.org/licenses/
+# The  above  copyright  notice  and  this  permission  notice  shall  be
+# included  in  all  copies  or  substantial  portions  of  the  Software.
+#
+# THE  SOFTWARE  IS  PROVIDED  ,  WITHOUT  WARRANTY  OF ANY KIND,
+# EXPRESS OR IMPLIED,  INCLUDING  BUT NOT LIMITED  TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE  FOR ANY
+# CLAIM,  DAMAGES OR OTHER LIABILITY,  WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE,  ARISING FROM,  OUT OF  OR IN  CONNECTION WITH THE
+# SOFTWARE   OR   THE   USE   OR   OTHER   DEALINGS  IN   THE  SOFTWARE.
 
 """Tests for ``human-friendly_pedantic-timedelta``."""
 
