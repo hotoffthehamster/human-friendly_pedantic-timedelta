2a6a83fd455bcdb6080693617880b818d21fd03c
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-03-29 02:02:17 -0500

    Update: Use pre-abbreviation options time units by default.

--- pedantic_timedelta/__init__.py
+++ pedantic_timedelta/__init__.py
@@ -86,22 +86,23 @@ class PedanticTimedelta(timedelta):
     """SECS_IN_YEAR / 12.0"""
 
     UNIT_NAMES = {
-        'year':   (_('year'),   _('y'), _('yr'), _('yēr'), _('yrs.'), _('yr') ), # noqa
-        'month':  (_('month'),  _('m'), _('mo'), _('mon'), _('mos.'), _('mon')), # noqa
-        'day':    (_('day'),    _('d'), _('dā'), _('day'), _('days'), _('day')), # noqa
-        'hour':   (_('hour'),   _('H'), _('hr'), _('our'), _('hrs.'), _('hr') ), # noqa
-        'minute': (_('minute'), _('M'), _('m.'), _('min'), _('mins'), _('min')),
-        'second': (_('second'), _('S'), _('s.'), _('sec'), _('secs'), _('sec')),
+        'year':   (_('year'),   _('y'), _('yr'), _('yēr'), _('yrs.'), _('yr'),  _('year')),  # noqa
+        'month':  (_('month'),  _('m'), _('mo'), _('mon'), _('mos.'), _('mon'), _('month')), # noqa
+        'day':    (_('day'),    _('d'), _('dā'), _('day'), _('days'), _('day'), _('day')),   # noqa
+        'hour':   (_('hour'),   _('H'), _('hr'), _('our'), _('hrs.'), _('hr'),  _('hour')),  # noqa
+        'minute': (_('minute'), _('M'), _('m.'), _('min'), _('mins'), _('min'), _('min')),
+        'second': (_('second'), _('S'), _('s.'), _('sec'), _('secs'), _('sec'), _('sec')),
     }
     UNIT_NAME_FULL = 0
     UNIT_NAME_ONECH = 1
     UNIT_NAME_TWOCH = 2
     UNIT_NAME_TREYWIDE = 3
     UNIT_NAME_FOURWIDE = 4
-    UNIT_NAME_ABBREV = 5
+    UNIT_NAME_BRIEF = 5
+    UNIT_NAME_ABBREV = 6
     #
-    UNIT_NAME_INDEX_0 = UNIT_NAME_FULL
-    UNIT_NAME_INDEX_N = UNIT_NAME_ABBREV
+    UNIT_NAME_INDEX_0 = 0
+    UNIT_NAME_INDEX_N = len(UNIT_NAMES['year']) - 1
 
     # ***
 
@@ -276,9 +277,11 @@ class PedanticTimedelta(timedelta):
         return tdw.time_format_scaled()[0]
 
     def _validate_abbreviate(self, abbreviate=None):
+        if abbreviate is None:
+            # This is how code worked before abbrevs were added.
+            return PedanticTimedelta.UNIT_NAME_ABBREV
         if (
-            abbreviate is None
-            or abbreviate < PedanticTimedelta.UNIT_NAME_INDEX_0
+            abbreviate < PedanticTimedelta.UNIT_NAME_INDEX_0
             or abbreviate > PedanticTimedelta.UNIT_NAME_INDEX_N
         ):
             return PedanticTimedelta.UNIT_NAME_FULL
@@ -318,17 +321,20 @@ class PedanticTimedelta(timedelta):
         lkup_unit, s_scale = self._determine_unit_and_scale()
         abbreviate = self._validate_abbreviate(abbreviate)
         tm_unit = PedanticTimedelta.UNIT_NAMES[lkup_unit][abbreviate]
-        return tm_unit, s_scale, abbreviate
+        return tm_unit, s_scale, lkup_unit, abbreviate
 
-    def _pluralize_periodify(self, adj_time, tm_unit, abbreviate=None):
+    def _pluralize_periodify(self, adj_time, tm_unit, lkup_unit, abbreviate):
         if abbreviate in (
             PedanticTimedelta.UNIT_NAME_FULL,
+            PedanticTimedelta.UNIT_NAME_BRIEF,
             PedanticTimedelta.UNIT_NAME_ABBREV,
         ):
             # (lb): I timeit'd Inflector().pluralize vs. inflectr=Inflector();
             # inflectr.pluralize. Creating object ahead of time is not faster.
             tm_units = Inflector(English).conditional_plural(adj_time, tm_unit)
-            if abbreviate == PedanticTimedelta.UNIT_NAME_ABBREV:
+            abbrev_full_i = PedanticTimedelta.UNIT_NAME_FULL
+            unabbreviated = PedanticTimedelta.UNIT_NAMES[lkup_unit][abbrev_full_i]
+            if tm_unit != unabbreviated:
                 tm_units += '.'
         else:
             tm_units = tm_unit
@@ -343,12 +349,16 @@ class PedanticTimedelta(timedelta):
         expressed in seconds).
 
         :param field_width: Total field width, including decimal point.
+            Defaults to 0, i.e., no minimum width.
         :param precision: Time delta float value precision.
-        :param abbreviate: Special value indicating whether to abbreviate:
-          0 to not abbreviate;
-          1 to use single character;
-          2-4 to use 2, 3, or 4 character abbreviation, respectively; or
-          5 to use a 2 or 3 character abbreviation, e.g., hr, min, etc.
+            Default to 2, e.g., "3.14 days."
+        :param abbreviate: Integer to indicate whether to abbreviate:
+            0 to not abbreviate;
+            1 to use single character;
+            2-4 to use 2, 3, or 4 character abbreviation, respectively;
+            5 to use a 2 or 3 character abbreviation, not includings plural
+            and/or period, e.g., "1.00 yr.", "3.00 hrs.", "2.45 mins."
+            6 (Default) to use set: year, month, day, hour, min/min., sec/sec.
 
         :type field_width: int
         :type precision: int
@@ -360,9 +370,9 @@ class PedanticTimedelta(timedelta):
         >>> PedanticTimedelta(days=0.33).time_format_scaled()
         ('7.92 hours', 3600.0, 'hour')
         """
-        tm_unit, s_scale, abbreviate = self._units_and_scale(abbreviate)
+        tm_unit, s_scale, lkup_unit, abbreviate = self._units_and_scale(abbreviate)
         adj_time = self.total_seconds() / s_scale
-        tm_units = self._pluralize_periodify(adj_time, tm_unit, abbreviate)
+        tm_units = self._pluralize_periodify(adj_time, tm_unit, lkup_unit, abbreviate)
         template = '{{:{}.{}f}} {{}}'.format(field_width, precision)
         time_fmtd = (template.format(adj_time, tm_units))
         return time_fmtd, s_scale, tm_unit
